5b431a6cd63af1949e36f1f3c089a516728bb214
Masudha Meher <kasturimeh@gmail.com>
2022-01-29 13:59:02 +0530

    fixed: error handling

--- input/input.js
+++ input/input.js
@@ -3,13 +3,25 @@ const errorDiv = document.querySelector(".error");
 
 const regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
+const errorIcon = '<i class="fas fa-exclamation-circle"></i>';
+
+const enableError = () => {
+  emailInput.style.border = "#a52a2a 2px solid";
+  errorDiv.style.display = "block";
+};
+
+const disableError = () => {
+  emailInput.style.border = "#008000 2px solid";
+  errorDiv.style.display = "none";
+};
 emailInput.addEventListener("input", () => {
   let value = emailInput.value;
-  if (!regex.test(value) || value.length < 0) {
-    emailInput.style.border = "#a52a2a 2px solid";
-    errorDiv.style.display = "block";
-  } else {
-    emailInput.style.border = "#008000 2px solid";
-    errorDiv.style.display = "none";
-  }
+  
+  if (value.length <= 0) {
+    errorDiv.innerHTML = `${errorIcon} This field cannot be empty`;
+    enableError();
+  } else if (!regex.test(value)) {
+    errorDiv.innerHTML = `${errorIcon} Not a valid email address`;
+    enableError();
+  } else disableError();
 });

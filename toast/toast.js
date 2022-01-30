8ef2dddd3b4362e1147ef7d96402170e6f62e588
Masudha Meher <kasturimeh@gmail.com>
2022-01-31 00:18:23 +0530

    add: js to toast

--- toast/toast.js
+++ toast/toast.js
@@ -1,13 +1,13 @@
-// const baselineBtn = document.querySelector(".baseline-btn");
-// const baselineToast = document.querySelector(".baseline-toast");
-// const toastBtn = document.querySelector(".toast-btn");
+const baselineBtn = document.querySelector(".baseline-btn");
+const baselineToast = document.querySelector(".baseline-toast");
+const toastBtn = document.querySelector(".toast-btn");
 
-// const hideToast = () => (baselineToast.style.visibility = "hidden");
-// const showToast = () => {
-//   baselineToast.style.visibility = "visible";
-//   setTimeout(hideToast, 5000);
-// };
+const hideToast = () => (baselineToast.style.visibility = "hidden");
+const showToast = () => {
+  baselineToast.style.visibility = "visible";
+  setTimeout(hideToast, 5000);
+};
 
-// baselineBtn.addEventListener("click", showToast);
+baselineBtn.addEventListener("click", showToast);
 
-// toastBtn.addEventListener("click", hideToast);
+toastBtn.addEventListener("click", hideToast);
